9d7f63d77b609b034fd07daa5f5f1983705e3b37
bekobsi <naoya97316@gmail.com>
2020-01-30 21:29:58 +0900

    initial commit

--- personal APP/MemberAdditionViewController.swift
+++ personal APP/MemberAdditionViewController.swift
@@ -7,21 +7,24 @@
 //
 import UIKit
 //---------変数の定義-------------
+
 var gender = "男性"
 var names:Array<String> = []
 var huriganas:Array<String> = []
 let userDefaults = UserDefaults.standard
 
-class MemberAdditionViewController: UIViewController ,UITextFieldDelegate{
+class MemberAdditionViewController: UIViewController ,UITextFieldDelegate,UITableViewDelegate,UITableViewDataSource{
+//    曜日取得関連の定義
+    var weekday:[String] = ["月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"]
+    var rowListArray = [Int]()
+    var itemListArray = [Int]()
+    var sortArray = [Int]()
+
     // キーボードを閉じる
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         fullNameAdd.delegate = self
@@ -32,20 +35,65 @@ class MemberAdditionViewController: UIViewController ,UITextFieldDelegate{
         huriganaAdd.placeholder = "スペースを入れずに入力"
         huriganaAdd.returnKeyType = .done
         huriganaAdd.delegate = self
-        
-        
-        
-        
-        
-        
-        
+//-------------tableViewを許可--------
+        tableView.dataSource = self
+          tableView.delegate = self
+          tableView.allowsMultipleSelection = true
         // Do any additional setup after loading the view.
     }
     //-----------ふりがなを入力するtextField--------
     @IBOutlet weak var huriganaAdd: UITextField!
     //-----------名前のtextField-----------------
     @IBOutlet weak var fullNameAdd: UITextField!
+//-----------tableView-----------------
+    @IBOutlet weak var tableView: UITableView!
     
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weekday.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let Cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        Cell.textLabel?.text = weekday[indexPath.row]
+        
+            Cell.backgroundColor = UIColor.clear
+                //  セルの選択時の背景色を消す
+                Cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        
+                //  セルの選択状況の判定
+                if (rowListArray.contains(indexPath.row)){
+                    Cell.accessoryType = .checkmark
+                }else{
+                    Cell.accessoryType = .none
+                }
+                return Cell
+            }
+
+        //    選択時のの動作
+            func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+                let cell = tableView.cellForRow(at: indexPath)
+                //  選択したセルにチェックマークが無い場合
+                if(cell?.accessoryType == UITableViewCell.AccessoryType.none){
+                    cell?.accessoryType = .checkmark
+                    self.rowListArray.append(indexPath.row)
+                }else{
+                //  選択したセルにチェックマークがある場合
+                    cell?.accessoryType = .none
+                    let  listNumber = rowListArray.filter ({ (n:Int) -> Bool in
+                        if n != indexPath.row{
+                            return true
+                        }else{
+                            return false
+                        }})
+                    rowListArray = listNumber
+                }
+
+                //  配列を昇順で並び替え、
+                //  都道府県コードに変換する。
+                sortArray = rowListArray.sorted{$0 < $1}
+                itemListArray = sortArray.map{$0 + 1}
+                print(itemListArray)
+            }
     
     //-----------性別を設定するSegmentControlボタン
     @IBAction func genderButton(_ sender: UISegmentedControl) {
@@ -149,15 +197,9 @@ class MemberAdditionViewController: UIViewController ,UITextFieldDelegate{
             
         }
     }
+//-------------ここまでアラート表示メソッド------------------------
     
-    
-    //-------------ここまでアラート表示メソッド------------------------
-    
-    
-    
-    
-    
-    //------------キーボード以外を押すとキーボードが下がる機能-----------------
+//------------キーボード以外を押すとキーボードが下がる機能-----------------
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
